9b2ba63a60e48e85bcb25982cbc53166761e79da
Brian A. Danielak <briandaniela.k+github@gmail.com>
2019-04-11 15:31:19 -0400

    Build book PDF using Eisvogel Template
    https://github.com/Wandmalfarbe/pandoc-latex-template

--- _build.sh
+++ _build.sh
@@ -2,7 +2,29 @@
 
 set -ev
 
+BOOK_FILE='fullstack-textbook'
+BOOK_TITLE='Building Full-Stack Web Applications'
+AUTHOR='Fullstack Academy of Code'
+
+Rscript -e "bookdown::render_book('index.Rmd', 'rmarkdown::md_document')"
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
-Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book')"
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::epub_book')"
 
+pandoc  \
+  $BOOK_FILE.md \
+  -o $BOOK_FILE.tex \
+  --from markdown+yaml_metadata_block
+
+pandoc \
+  $BOOK_FILE.tex \
+  -o $BOOK_FILE.pdf \
+  --from latex \
+  --template eisvogel-template.tex \
+  --table-of-contents \
+  --variable=titlepage:true \
+  --variable=toc_own_page:false \
+  --variable=title:"$BOOK_TITLE" \
+  --variable=author:"$AUTHOR" \
+  --variable=titlepage-color:"435488" \
+  --variable=titlepage-text-color:"FFFFFF" \
+  --variable=titlepage-rule-color:"FFFFFF"
